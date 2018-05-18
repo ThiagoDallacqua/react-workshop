4a2a6b0cfb0cc173f3251286096550465fa4ae1d
Thiago Dallacqua <thiagodallacqua@outlook.com>
2018-05-18 12:14:54 -0300

    Added regular application template
    placed the regular HTML to this application
    pasted the application base css temporarely to a .css file

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import './temp.css'
 
 class App extends Component {
   render() {
     return (
-      <h1>Welcome to React</h1>
+      <div id="container">
+
+          <h1>To-Do List<i className="fa fa-plus" ariaHidden="true" /></h1>
+
+          <input type="text" placeholder="Add New Todo" />
+
+          <ul>
+              <li>
+                <span className="delete">
+                  <i className="fa fa-trash-o" ariaHidden="true" />
+                </span>
+                <span className="completedMobile">
+                  <i className='fa fa-check' aria-hidden='true' />
+                </span>
+                Go to potions class
+              </li>
+              <li>
+                <span className="delete">
+                  <i className="fa fa-trash-o" ariaHidden="true" />
+                </span>
+                <span className="completedMobile">
+                  <i className='fa fa-check' aria-hidden='true' />
+                </span>
+                Buy new robes
+              </li>
+              <li>
+                <span className="delete">
+                  <i className="fa fa-trash-o" ariaHidden="true" />
+                </span>
+                <span className="completedMobile">
+                  <i className='fa fa-check' aria-hidden='true'/>
+                </span>
+                Visit Hagrid
+              </li>
+          </ul>
+      </div>
     );
   }
 }
