0489744c557adc1f2bb56a496fe19f380dab9f25
Thiago Dallacqua <thiagodallacqua@outlook.com>
2018-05-18 17:00:12 -0300

    started componetization

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,45 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './temp.css'
+import styled from 'styled-components'
+import Header from './components/Header'
 
-class App extends Component {
+const Container = styled.section`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TodoList = styled.div`
+  width: 360px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
+  background: #f7f7f7;
+`;
+
+class  App extends React.Component {
+  state = {
+    title: 'todo-list'
+  }
+
   render() {
+    const { title } = this.state
+
     return (
-      <div id="container">
+      <Container>
+          <TodoList>
+            <Header title={title} />
 
-          <h1>To-Do List<i className="fa fa-plus" ariaHidden="true" /></h1>
+            <input type="text" placeholder="Add New Todo" />
 
-          <input type="text" placeholder="Add New Todo" />
-
-          <ul>
-              <li>
-                <span className="delete">
-                  <i className="fa fa-trash-o" ariaHidden="true" />
-                </span>
-                <span className="completedMobile">
-                  <i className='fa fa-check' aria-hidden='true' />
-                </span>
-                Go to potions class
-              </li>
-              <li>
-                <span className="delete">
-                  <i className="fa fa-trash-o" ariaHidden="true" />
-                </span>
-                <span className="completedMobile">
-                  <i className='fa fa-check' aria-hidden='true' />
-                </span>
-                Buy new robes
-              </li>
-              <li>
-                <span className="delete">
-                  <i className="fa fa-trash-o" ariaHidden="true" />
-                </span>
-                <span className="completedMobile">
-                  <i className='fa fa-check' aria-hidden='true'/>
-                </span>
-                Visit Hagrid
-              </li>
-          </ul>
-      </div>
+            <ul>
+                <li>
+                  <span className="delete">
+                    <i className="fa fa-trash-o" ariaHidden="true" />
+                  </span>
+                  <span className="completedMobile">
+                    <i className='fa fa-check' aria-hidden='true' />
+                  </span>
+                  Go to potions class
+                </li>
+                <li>
+                  <span className="delete">
+                    <i className="fa fa-trash-o" ariaHidden="true" />
+                  </span>
+                  <span className="completedMobile">
+                    <i className='fa fa-check' aria-hidden='true' />
+                  </span>
+                  Buy new robes
+                </li>
+                <li>
+                  <span className="delete">
+                    <i className="fa fa-trash-o" ariaHidden="true" />
+                  </span>
+                  <span className="completedMobile">
+                    <i className='fa fa-check' aria-hidden='true'/>
+                  </span>
+                  Visit Hagrid
+                </li>
+            </ul>
+          </TodoList>
+      </Container>
     );
   }
 }
---
+++ react-todo/src/components/Header/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const rotateRight = keyframes`
+  0%{
+
+      transform: rotate(0deg);
+  }100%{
+
+      transform: rotate(90deg) scale(1.5);
+  }
+`;
+
+const rotateLeft = keyframes`
+  0%{
+
+      transform: rotate(90deg) scale(1.5);
+  }100%{
+
+      transform: rotate(0deg) scale(1);
+  }
+`;
+
+const rotateFast = keyframes`
+  0%{
+
+      transform: rotate(0deg) scale(1.5);
+  }100%{
+
+      transform: rotate(540deg) scale(1.5);
+  }
+`;
+
+const Title = styled.h1`
+  background: #2980b9;
+  color: white;
+  margin: 0;
+  padding: 10px 20px;
+  text-transform: uppercase;
+  font-size: 24px;
+  font-weight: normal;
+`;
+
+const AddButton = styled.i`
+  cursor: pointer;
+  animation: ${({enter, leave, clicked}) => enter ? rotateRight : leave ? rotateLeft : clicked ? rotateFast : ''} .5s ease;
+  padding: 5px;
+  &:hover{
+    transform: scale(1.5);
+  }
+`;
+
+class Header extends React.Component {
+  state = {
+    enter: false,
+    leave: false,
+    clicked: false
+  }
+
+  onMouseEnter = () => this.setState({ enter: true, leave: false })
+
+  onMouseLeave = () => this.setState({ enter: false, leave: true })
+
+  onClick = () => this.setState({ enter: false, leave: false, clicked: true })
+
+  render() {
+    const { enter, leave, clicked } = this.state
+    const { title } = this.props
+
+    return (
+      <Title>
+        {title}
+        <AddButton
+          className="fa fa-plus"
+          ariaHidden="true"
+          enter={ enter }
+          leave={ leave }
+          clicked={ clicked }
+          onClick={this.onClick}
+          onMouseEnter={this.onMouseEnter}
+          onMouseLeave={this.onMouseLeave}
+        />
+      </Title>
+    )
+  }
+}
+
+export default Header
